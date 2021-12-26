6ff471c24b292a659cf5107d24c9a8f670687034
gabiayako <gabiayako@gmail.com>
2021-12-26 18:52:25 -0300

    Click on logo redirects to home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Header() {
   return (
-    <Image
-      src="/images/logo.svg"
-      alt="logo"
-      width={238}
-      height={25}
-      objectFit="contain"
-    />
+    <Link href="/">
+      <Image
+        src="/images/logo.svg"
+        alt="logo"
+        width={238}
+        height={25}
+        objectFit="contain"
+      />
+    </Link>
   );
 }
